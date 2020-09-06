e83db653602075203e7943288ad3c2449da0f96c
prass07 <prasup07@gmail.com>
2020-09-06 22:05:26 +0530

    completed back-end

---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const knex = require('knex');
+const { response } = require('express');
+
+const db = knex({
+        client: 'pg',
+        connection: {
+          host : '127.0.0.1',
+          user : 'prass',
+          password : '0327',
+          database : 'myapp'
+        }
+      });
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const database = {
+    users: [
+        {
+            id:'1',
+            username: 'john',
+            password: '123',
+        }
+    ]
+}
+
+app.get('/', (req,res)=>{
+    res.send(database.users);
+})
+
+app.post('/home',(req,res) => {
+    if(req.body.username === 'admin' && req.body.password === 'pass'){
+        res.json('succes');
+    }else {
+        res.status(400).json('error logging in');
+    }
+})
+
+app.post('/adduser',(req,res)=>{
+    const {username, password} = req.body;
+    db('users')
+        .returning('*')
+        .insert({
+            username: username,
+            password: password
+        })
+            .then(user => {
+                res.json(user[0]);
+            })
+            .catch(err => res.status(400).json('unable to adduser'))
+})
+
+app.get('/view-users-page',(req,res)=>{
+    db.select('*').from('users')
+    .then(users =>{
+        res.json(users)
+    })
+    
+})
+
+app.listen(3000, ()=> {
+    console.log('app is running on port 3000'); 
+})
+
